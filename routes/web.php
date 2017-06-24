c4b56c4ab6cfe8ce26c2f0198585852425b8531d
Jason O'Brien <jasonobrien@jasons-mbp.attlocal.net>
2017-06-24 19:58:34 -0400

    through part 6 of 12 of exercise

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,24 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
+
+/*
+|--------------------------------------------------------------------------
+| Example Code
+|--------------------------------------------------------------------------
+  Dynamic Route Example
+
+  Route::get('/users/{id}/{name}', function($id, $name) {
+    return 'This is user ' . $name. ' with an id of '.$id;
+  });
+|
+*/
+
+Route::get('/', 'PagesController@index');
+
+Route::get('/about', 'PagesController@about');
+
+Route::get('/services', 'PagesController@services');
+
+Route::resource('posts', 'PostsController');
