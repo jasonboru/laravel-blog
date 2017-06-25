cd0afa1f51d3ab975b8bd48fe3815404a8d9eaf4
Jason O'Brien <jasonobrien@jasons-mbp.attlocal.net>
2017-06-24 23:19:00 -0400

    fleshed out edit, update and destroy methods

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -78,7 +78,8 @@ class PostsController extends Controller
      */
     public function edit($id)
     {
-        //
+        $post = Post::find($id);
+        return view('posts.edit')->with('post', $post);
     }
 
     /**
@@ -90,7 +91,18 @@ class PostsController extends Controller
      */
     public function update(Request $request, $id)
     {
-        //
+        $this->validate($request, [
+          'title' => 'required',
+          'body' => 'required'
+        ]);
+
+        // Create Posts
+        $post = Post::find($id);
+        $post->title = $request->input('title');
+        $post->body = $request->input('body');
+        $post->save();
+
+        return redirect('/posts')->with('success', 'Post Updated');
     }
 
     /**
@@ -101,6 +113,11 @@ class PostsController extends Controller
      */
     public function destroy($id)
     {
-        //
+        $post = Post::find($id);
+        $post->delete();
+
+        return redirect('/posts')->with('success', 'Post Removed');
+
+
     }
 }
