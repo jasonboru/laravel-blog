b8d1068cd684a1f0f375a7fc77ac93517d7ebf36
Jason O'Brien <jasonobrien@jasons-mbp.attlocal.net>
2017-07-12 10:14:49 -0400

    added crop_name & strain to show on posts index

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -6,11 +6,12 @@
         @foreach($posts as $post)
           <div class="well">
             <div class="row">
-              <div class="col-md-4 col-sm-4">
-                <img style="width:100%" src="/storage/cover_images/{{$post->cover_image}}">
+              <div class="col-md-3 col-sm-3">
+                <img style="height:120px" src="/storage/cover_images/{{$post->cover_image}}">
               </div>
-              <div class="col-md-8 col-sm-8">
-                <h3><a href="/posts/{{$post->id}}">{{$post->title}}</a></h3>
+              <div class="col-md-9 col-sm-9">
+                <h3><a href="/posts/{{$post->id}}">{{$post->crop_name}}</a></h3>
+                <h4>{{$post->strain}}</h4>
                 <small>Written on {{$post->created_at}} by {{$post->user->name}}</small>
               </div>
             </div>
