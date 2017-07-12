336e4945fb7091940dc29d0640f7938676447580
Jason O'Brien <jasonobrien@jasons-mbp.attlocal.net>
2017-07-12 13:23:48 -0400

    removed unused code, changed sitename to go to dashboard

--- resources/views/inc/navbar.blade.php
+++ resources/views/inc/navbar.blade.php
@@ -1,28 +1,4 @@
-<!--<nav class="navbar navbar-inverse">
-  <div class="container">
-    <div class="navbar-header">
-      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
-        <span class="sr-only">Toggle navigation</span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-      </button>
-      <a class="navbar-brand" href="/">{{config('app.name', 'LSAPP')}}</a>
-    </div>
-    <div id="navbar" class="navbar-collapse collapse">
-      <ul class="nav navbar-nav">
-            <li><a href="/">Home</a></li>
-            <li><a href="/about">About</a></li>
-            <li><a href="/services">Services</a></li>
-            <li><a href="/posts">Blog</a></li>
-      </ul>
-      <ul class="nav navbar-nav navbar-right">
 
-      </ul>
-
-        </div>
-      </div>
-    </nav> -->
 
     <nav class="navbar navbar-inverse">
         <div class="container">
@@ -37,7 +13,7 @@
                 </button>
 
                 <!-- Branding Image -->
-                <a class="navbar-brand" href="{{ url('/') }}">
+                <a class="navbar-brand" href="{{ url('/dashboard') }}">
                     {{ config('app.name', 'Laravel') }}
                 </a>
             </div>
@@ -51,7 +27,7 @@
                 <ul class="nav navbar-nav">
                       <li><a href="/">Home</a></li>
                       <li><a href="/about">About</a></li>
-                      <li><a href="/posts">Blog</a></li>
+                      <li><a href="/posts">All Grows</a></li>
                 </ul>
 
                 <!-- Right Side Of Navbar -->
