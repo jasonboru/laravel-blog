9a170d8be650aa52f79296dda42d589772b08cb1
Jason O'Brien <jasonobrien@jasons-mbp.attlocal.net>
2017-07-12 10:12:03 -0400

    added crop_name and strain to the create post form

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -1,12 +1,16 @@
 @extends('layouts.app')
 
 @section('content')
-  <h1>Create Post</h1>
+  <h1>Create New Grow</h1>
 
   {!! Form::open(['action' => 'PostsController@store', 'method' => 'POST', 'enctype' => 'multipart/form-data']) !!}
     <div class="form-group">
-      {{Form::label('title', 'Title')}}
-      {{Form::text('title', '', ['class' => 'form-control', 'placeholder' => 'Title'])}}
+      {{Form::label('crop_name', 'Crop Name')}}
+      {{Form::text('crop_name', '', ['class' => 'form-control', 'placeholder' => 'Crop Name'])}}
+    </div>
+    <div class="form-group">
+      {{Form::label('strain', 'Strain')}}
+      {{Form::text('strain', '', ['class' => 'form-control', 'placeholder' => 'Strain'])}}
     </div>
     <div class="form-group">
       {{Form::label('body', 'Body')}}
