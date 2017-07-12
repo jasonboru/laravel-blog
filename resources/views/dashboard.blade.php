582faba3d0c261973f9393871a0255d8be42363d
Jason O'Brien <jasonobrien@jasons-mbp.attlocal.net>
2017-07-12 10:07:20 -0400

    added cover_image, crop_name & strain to dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -9,17 +9,18 @@
 
                 <div class="panel-body">
                     <a href="/posts/create" class="btn btn-primary">Create Post</a>
-                    <h3>Your Blog Posts</h3>
+                    <h3>Your Grow Posts</h3>
                     @if(count($posts) > 0)
                     <table class="table table-striped">
                       <tr>
-                        <th>Title</th>
+                        <th>Grows</th>
                         <th></th>
                         <th></th>
                       </tr>
                       @foreach($posts as $post)
                         <tr>
-                          <td>{{$post->title}}</td>
+                          <!--<td><img style="width:10%" src="/storage/cover_images/{{$post->cover_image}}"></td> -->
+                          <td>{{$post->crop_name}} - {{$post->strain}}</td>
                           <td><a href="/posts/{{$post->id}}/edit" class="btn btn-default">Edit</a></td>
                           <td>
                             {!!Form::open(['action' => ['PostsController@destroy', $post->id], 'method' => 'POST', 'class' => 'pull-right'])!!}
