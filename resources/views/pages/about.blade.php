c105db9b2b200eba39a5360c8189b9c18e116ddc
Jason O'Brien <jasonobrien@jasons-mbp.attlocal.net>
2017-07-12 10:09:33 -0400

    added copy to the about page

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -2,5 +2,6 @@
 
 @section('content')
       <h1><?php echo $title; ?></h1>
-      <p>This is the about page.</p>
+      <p>GrowShow is an app to help you journal and share your grow from seedling to harvest. Keep track of week to week statistics to help keep track of what went right and what went wrong.</p>
+      <p>Share your posts with other growers and checkout other peoples grows for ideas and information. Leave comments and conect to make you and your garden grow.</p>
 @endsection
