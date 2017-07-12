84cc3b45d7882af83d546e39a583e9d38ebe4711
Jason O'Brien <jasonobrien@jasons-mbp.attlocal.net>
2017-07-12 10:17:12 -0400

    added route for weeks

--- routes/web.php
+++ routes/web.php
@@ -29,9 +29,9 @@ Route::get('/', 'PagesController@index');
 
 Route::get('/about', 'PagesController@about');
 
-Route::get('/services', 'PagesController@services');
-
 Route::resource('posts', 'PostsController');
+
+Route::resource('weeks', 'WeeksController');
 
 Auth::routes();
 
