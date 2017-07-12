8306191287c550ab908689f201f5642c8d3b8380
Jason O'Brien <jasonobrien@jasons-mbp.attlocal.net>
2017-07-12 10:05:19 -0400

    added crop_name and strain to Post migrations

--- database/migrations/2017_06_24_230738_create_posts_table.php
+++ database/migrations/2017_06_24_230738_create_posts_table.php
@@ -15,7 +15,8 @@ class CreatePostsTable extends Migration
     {
         Schema::create('posts', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('title');
+            $table->string('crop_name');
+            $table->string('strain');
             $table->mediumText('body');
             $table->timestamps();
         });
