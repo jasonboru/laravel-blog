f64b8ea72d85a839bcb53a98b1ac7e66f1648fe2
Jason O'Brien <jasonobrien@jasons-mbp.attlocal.net>
2017-07-12 11:05:57 -0400

    added create and store methods for Weeks

--- app/Http/Controllers/WeeksController.php
+++ app/Http/Controllers/WeeksController.php
@@ -3,6 +3,9 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
+use App\Week;
+use DB;
 
 class WeeksController extends Controller
 {
@@ -23,7 +26,7 @@ class WeeksController extends Controller
      */
     public function create()
     {
-        //
+        return view('weeks.create');
     }
 
     /**
@@ -34,7 +37,46 @@ class WeeksController extends Controller
      */
     public function store(Request $request)
     {
-        //
+      $this->validate($request, [
+        'week_num' => 'required',
+        'nutrient' => 'required',
+        'dosage' => 'required',
+        'temperature' => 'required',
+        'humidity' => 'required',
+
+      ]);
+
+      /* code to add image later
+
+      //Handle File upload
+      if($request->hasFile('cover_image')){
+        // Get filename with the extension
+        $filenameWithExt = $request->file('cover_image')->getClientOriginalName();
+        // Get just filename
+        $filename = pathinfo($filenameWithExt, PATHINFO_FILENAME);
+        // Get just extension
+        $extension = $request->file('cover_image')->getClientOriginalExtension();
+        // Filename to Store
+        $fileNameToStore = $filename. '_' .time().'.'.$extension;
+        // Upload image
+        $path = $request->file('cover_image')->storeAs('public/cover_images', $fileNameToStore);
+      } else {
+        $fileNameToStore = 'noimage.jpg';
+      }
+
+      */
+
+      // Create Posts
+      $week = new Week;
+      $week->week_num = $request->input('week_num');
+      $week->nutrient = $request->input('nutrient');
+      $week->dosage = $request->input('dosage');
+      $week->temperature = $request->input('temperature');
+      $week->humidity = $request->input('humidity');
+      $week->notes = $request->input('notes');
+      $week->save();
+
+      return redirect('/posts')->with('success', 'Week Created');
     }
 
     /**
