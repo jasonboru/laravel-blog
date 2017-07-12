de4a2e44a4a7899e518191079ba998f3a4dafb03
Jason O'Brien <jasonobrien@jasons-mbp.attlocal.net>
2017-07-12 11:08:58 -0400

    added href for add week btn

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -14,10 +14,35 @@
 
   <small>Written on {{$post->created_at}} by {{$post->user->name}}</small>
   <hr>
+
+  {{-- add in the weeks for each post
+
+  <h1>weeks</h1>
+    @if(count($weeks) > 0)
+        @foreach($weeks as $week)
+          <div class="well">
+            <div class="row">
+              <div class="col-md-3 col-sm-3">
+                <img style="height:120px" src="/storage/cover_images/{{$week->cover_image}}">
+              </div>
+              <div class="col-md-9 col-sm-9">
+                <h3><a href="/weeks/{{$week->id}}">{{$week->week_num}}</a></h3>
+                <small>Written on {{$week->created_at}}</small>
+              </div>
+            </div>
+
+          </div>
+        @endforeach
+      @else
+        <p>No weeks found</p>
+      @endif
+
+  --}}
+
   @if(!Auth::guest())
       @if (Auth::user()->id == $post->user_id)
         <a href="/posts/{{$post->id}}/edit" class="btn btn-default">Edit</a>
-        <a href="" class="btn btn-default">Add Week</a>
+        <a href="/weeks/create" class="btn btn-primary">Add Week</a>
 
         {!!Form::open(['action' => ['PostsController@destroy', $post->id], 'method' => 'POST', 'class' => 'pull-right'])!!}
             {{Form::hidden('_method', 'DELETE')}}
