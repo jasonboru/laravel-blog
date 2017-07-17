633db31bfbc2fdad1cd77103ee611ba99ed72853
Jason O'Brien <jasonobrien@android-910ae20177f90bfd.local.valenciacc.edu>
2017-07-17 18:45:23 -0400

    added bbDelete class to delete button

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -24,7 +24,7 @@
                           <td><a href="/posts/{{$post->id}}">{{$post->crop_name}}</a> - {{$post->strain}}</td>
                           <td><a href="/posts/{{$post->id}}/edit" class="btn btn-default">Edit</a></td>
                           <td>
-                            {!!Form::open(['action' => ['PostsController@destroy', $post->id], 'method' => 'POST', 'class' => 'pull-right'])!!}
+                            {!!Form::open(['action' => ['PostsController@destroy', $post->id], 'method' => 'POST', 'class' => 'pull-right bbDelete'])!!}
                                 {{Form::hidden('_method', 'DELETE')}}
                                 {{Form::submit('Delete', ['class' => 'btn btn-danger'])}}
                             {!!Form::close()!!}
