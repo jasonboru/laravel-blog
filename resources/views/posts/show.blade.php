763c47193e3acd96de847f83c931c70e60b5088e
Jason O'Brien <jasonobrien@jasons-mbp.attlocal.net>
2017-07-19 13:37:12 -0400

    started styling post.show

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -1,56 +1,69 @@
 @extends('layouts.app')
 
 @section('content')
-  <a href="/posts" class="btn btn-default">Go Back</a>
-  <h1>{{$post->crop_name}}</h1>
-  <h2>{{$post->strain}}</h2>
+  <a href="/posts" class="btn btn-lg btn-primary goBack">Go Back</a>
+  <div class="overlay">
+    <div class="row">
+      <div class="col-sm-6">
+        <h1>{{$post->crop_name}}</h1>
+      </div>
+      <div class="col-sm-6">
+        <small class="pull-right">Written on {{$post->created_at->format('m-d-Y')}} by {{$post->user->name}}</small>
+      </div>
+
+    </div>
+
+  <hr>
+  <h2 class="strain">{{$post->strain}}</h2>
 
   <div class="row">
     <div class="col-md-6 col-sm-6">
       <a href="https://s3.amazonaws.com/final-project-growshow/uploads/{{$post->cover_image}}" data-lightbox="{{$post->cover_image}}" data-title="{{$post->crop_name}} - {{$post->strain}}">
-        <img style="width:100%" src="https://s3.amazonaws.com/final-project-growshow/uploads/{{$post->cover_image}}">
+        <img class="CropImg" style="width:100%" src="https://s3.amazonaws.com/final-project-growshow/uploads/{{$post->cover_image}}">
       </a>
     </div>
     <div class="col-md-6 col-sm-6">
-      <div class="row">
-        <div class="col-md-3 col-sm-3">
-          <h4>Method:</h4>
-        </div>
-        <div class="col-md-9 col-sm-9">
-          <div class="well"> {{$post->method}}</div>
-        </div>
-      </div>
+      <div class="well">
+          <div class="row">
+            <div class="col-md-4 col-sm-4">
+              <h3>Method:</h3>
+            </div>
+            <div class="col-md-8 col-sm-8">
+              <div><h3 class="postInputResult">{{$post->method}}</h3></div>
+            </div>
+          </div>
 
-      <div class="row">
-        <div class="col-md-3 col-sm-3">
-          <h4>Location:</h4>
-        </div>
-        <div class="col-md-9 col-sm-9">
-          <div class="well"> {{$post->location}}</div>
-        </div>
-      </div>
+          <div class="row">
+            <div class="col-md-4 col-sm-4">
+              <h3>Location:</h3>
+            </div>
+            <div class="col-md-8 col-sm-8">
+              <div><h3 class="postInputResult"> {{$post->location}}</h3></div>
+            </div>
+          </div>
 
-      <div class="row">
-        <div class="col-md-3 col-sm-3">
-          <h4>Lighting:</h4>
-        </div>
-        <div class="col-md-9 col-sm-9">
-          <div class="well"> {{$post->lighting}}</div>
-        </div>
+          <div class="row">
+            <div class="col-md-4 col-sm-4">
+              <h3>Lighting:</h3>
+            </div>
+            <div class="col-md-8 col-sm-8">
+              <div><h3 class="postInputResult"> {{$post->lighting}}</h3></div>
+            </div>
+          </div>
       </div>
 
     </div>
   </div>
 
   <br><br>
-  <div>
-    {!!$post->body!!}
+  <h3>Notes:</h3>
+  <div class="well">
+    <span class="postNotes">{!!$post->body!!}</span>
   </div>
 
   <hr>
 
-  <small>Written on {{$post->created_at}} by {{$post->user->name}}</small>
-  <hr>
+
 
 
 
@@ -61,14 +74,14 @@
             <div class="row">
               <div class="col-md-3 col-sm-3">
                 <a href="/weeks/{{$week->id}}">
-                  <img style="width:100%" src="https://s3.amazonaws.com/final-project-growshow/uploads/{{$week->week_image}}">
+                  <img class="allWeekImg" style="width:100%" src="https://s3.amazonaws.com/final-project-growshow/uploads/{{$week->week_image}}">
                 </a>
               </div>
               <div class="col-md-9 col-sm-9">
                 <h3><a href="/weeks/{{$week->id}}">Week # {{$week->week_num}}</a></h3>
                 <div>{!!$week->notes!!}</div>
                 <hr>
-                <small>Written on {{$week->created_at}}</small>
+                <small>Written on {{$week->created_at->format('m-d-Y')}}</small>
               </div>
             </div>
 
@@ -80,8 +93,8 @@
 
     @if(!Auth::guest())
         @if (Auth::user()->id == $post->user_id)
-          <a href="/posts/{{$post->id}}/edit" class="btn btn-default">Edit</a>
-          <a href="/weeks/create?post={{ $post->id }}" class="btn btn-primary">Add Week</a>
+          <a href="/posts/{{$post->id}}/edit" class="btn btn-default">Edit Post</a>
+          <a href="/weeks/create?post={{ $post->id }}" class="btn btn-primary">Add a Week</a>
 
           {!!Form::open(['action' => ['PostsController@destroy', $post->id], 'method' => 'POST', 'class' => 'pull-right'])!!}
               {{Form::hidden('_method', 'DELETE')}}
@@ -114,6 +127,7 @@
       </div>
     </div>
 
+    <hr>
 
     <div class="row">
       <div id="comment-form" class="col-md-8 col-md-offset-2">
@@ -123,12 +137,12 @@
 
             <div class="col-md-6">
               {{ Form::label('name', "Name:") }}
-              {{ Form::text('name', null, ['class' => 'form-control']) }}
+              {{ Form::text('name', Auth::user()->name, ['class' => 'form-control']) }}
             </div>
 
             <div class="col-md-6">
               {{ Form::label('email', "Email:") }}
-              {{ Form::text('email', null, ['class' => 'form-control']) }}
+              {{ Form::text('email', Auth::user()->email, ['class' => 'form-control']) }}
             </div>
 
             <div class="col-md-12">
@@ -143,6 +157,8 @@
         {{ Form::close()}}
       </div>
     </div>
+
+  </div>
 
 
 
